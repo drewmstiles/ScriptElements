1cf317410a36587f45f42b59289018d6094b39e4
Drew Stiles <drewmstiles@gmail.com>
2016-03-30 21:58:30 -0700

    Implements constructors for Element class

--- src/components/Element.java
+++ src/components/Element.java
@@ -3,8 +3,36 @@ package components;
 public class Element
 {
 
-	public Element()
-	{
-		// TODO Auto-generated constructor stub
+	public enum Locator { ID, XPATH};
+
+	public static final String ID_LOCATOR = "id";
+	
+	public static final String XPATH_LOCATOR = "xpath";
+	
+	public Element(String id) {
+		this(Locator.ID, id);
 	}
+	
+	public Element(String locator, String location) {
+		this(convertToLocator(locator), location);
+	}
+	
+	public Element(Locator locator, String location) {
+		this.location = location;
+		this.locator = locator;
+	}
+	
+	private static Locator convertToLocator(String l) {
+		switch(l.toLowerCase()) {
+			case ID_LOCATOR:
+				return Locator.ID;
+			case XPATH_LOCATOR:
+				return Locator.XPATH;
+			default:
+				throw new RuntimeException("Locator \"" + l + "\" is not valid.");
+		}
+	}
+	
+	private String location;
+	private Locator locator;
 }
