a7063ba1660cd8d7e9acd300f7728a85ae9b7b28
Drew Stiles <drewmstiles@gmail.com>
2016-05-10 15:36:34 -0700

    Changes public find to list return

--- src/foundations/Script.java
+++ src/foundations/Script.java
@@ -1,5 +1,6 @@
 package foundations;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,29 +23,71 @@ import drivers.DriverFactory;
 
 public abstract class Script extends Thread
 {
-	public static final String TABLE_ROW_XPATH = ".//tr";
+	/*
+	 * Constructor
+	 */
 	
 	public Script(String b)
 	{
 		driver = DriverFactory.getDriverForBrowswer(b);
 	}
 	
-	public void scrollTo(Element e) {
+	
+	/*
+	 * Animation Methods
+	 */
+	
+	public void scrollTo(Element e) 
+	{
 		JavascriptManager.scrollTo(find(e), driver);
 	}
 	
-	public void goTo(String url) {
+	
+	/*
+	 * Navigation Methods
+	 */
+	
+	public void goTo(String url) 
+	{
 		driver.get(url);
 	}
+	
+	
+	/*
+	 * Location Methods
+	 */
 	
 	private WebElement find(Element e) {
 		return driver.findElement(By.xpath(e.getXpath()));
 	}
 	
+	
+	public List<Element> find(String xpath) 
+	{
+		List<WebElement> webElements = driver.findElements(By.xpath(xpath));
+		int numElements = webElements.size(); 
+		Element[] elements = new Element[numElements];
+		for (int i = 0; i < numElements; i++) 
+		{
+			String uniqueXPath = xpath + "[" + (i + 1) + "]"; // xpath indexing starts at 1
+			Element e = new Element(uniqueXPath);
+			WebElement we = webElements.get(i);
+			e.setText(we.getText());
+			elements[i] = e;
+		}
+		return Arrays.asList(elements);
+	}
+	
+	
 	public void waitFor(Element e) {
 		WebDriverWait wait = new WebDriverWait(driver, 60);
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(e.getXpath())));
 	}
+
+	
+	/*
+	 * Access Methods
+	 */
 	
 	public String[][] read(Table t) 
 	{
@@ -66,8 +109,8 @@ public abstract class Script extends Thread
 			JavascriptManager.highlight(r, driver);
 			// adding row of cells
 			String rowXPath = t.getXpath() + "//tr[" + (i + 1) + "]";
-			row.addAll(Arrays.asList(findElementsByXPath(rowXPath + "//th")));
-			row.addAll(Arrays.asList(findElementsByXPath(rowXPath + "//td")));
+			row.addAll(find(rowXPath + "//th"));
+			row.addAll(find(rowXPath + "//td"));
 			if (row.size() == 0)
 			{
 				// empty row
@@ -88,70 +131,19 @@ public abstract class Script extends Thread
 		// return
 	}
 	
-	public Table findTable(String tableXPath) 
-	{	
-		Table t = new Table(tableXPath);
-		appendToTable(t);
-		return t;
-		
-	}
-
 	
-	public String[][] appendToTable(Table t) 
+	public void write(String text, Element e) 
 	{
-		// initialize
-		ArrayList<String[]> data = new ArrayList<String[]>();
-		
-		// build
-		WebElement table = find(t);
-		List<WebElement> rows = table.findElements(By.xpath(TABLE_ROW_XPATH));
-		for (int i = 0; i < rows.size(); i++) 
-		{	
-			ArrayList<String> rowData = new ArrayList<String>();
-			ArrayList<Element> row = new ArrayList<Element>();
-			// get physical row
-			WebElement r = rows.get(i);
-			// animating row scroll
-			String color = JavascriptManager.getProperty("style.backgroundColor", r, driver);
-			JavascriptManager.scrollTo(r, driver);
-			JavascriptManager.highlight(r, driver);
-			// adding row of cells
-			String rowXPath = t.getXpath() + "//tr[" + (i + 1) + "]";
-			row.addAll(Arrays.asList(findElementsByXPath(rowXPath + "//th")));
-			row.addAll(Arrays.asList(findElementsByXPath(rowXPath + "//td")));
-			if (row.size() == 0)
-			{
-				// empty row
-			}
-			else 
-			{
-				// read text from table cell
-				for (int j = 0; j < row.size(); j++) {
-					rowData.add(row.get(j).getText());
-				}
-				data.add(rowData.toArray(new String[0]));
-			}
-			// animation cleanup
-			JavascriptManager.setColor(color, r, driver);
-		}
-		
-		return data.toArray(new String[0][0]);
-		// return
-	}
-	
-	
-	public void select(String text, DropDown dd) {
-		Select s = new Select(find(dd));
-		s.selectByVisibleText(text);	
-	}
-
-	
-	public void write(String text, Element e) {
 		find(e).sendKeys(text);
 	}
 	
 	
-	public void click(Element e) {
+	/*
+	 * Interaction Methods
+	 */
+	
+	public void click(Element e) 
+	{
 		
 		WebElement physical = find(e);
 		try 
@@ -172,33 +164,29 @@ public abstract class Script extends Thread
 		}
 	}
 	
+	
+	public void select(String text, DropDown dd) 
+	{
+		Select s = new Select(find(dd));
+		s.selectByVisibleText(text);	
+	}
+	
+	
 	public void switchToFrame(Element frame) 
 	{
 		driver.switchTo().frame(find(frame));
 	}
+	
 	
 	public void switchToDefaultFrame()
 	{
 		driver.switchTo().defaultContent();
 	}
 	
-	public Element[] findElementsByXPath(String xpath) 
-	{
-		List<WebElement> webElements = driver.findElements(By.xpath(xpath));
-		int numElements = webElements.size(); 
-		Element[] elements = new Element[numElements];
-		for (int i = 0; i < numElements; i++) 
-		{
-			String uniqueXPath = xpath + "[" + (i + 1) + "]"; // xpath indexing starts at 1
-			Element e = new Element(uniqueXPath);
-			WebElement we = webElements.get(i);
-			e.setText(we.getText());
-			elements[i] = e;
-		}
-		
-		return elements;
-	}
 	
+	/*
+	 * Exit Methods
+	 */
 	
 	public void end() {
 		driver.close();
@@ -207,6 +195,8 @@ public abstract class Script extends Thread
 	
 	@Override
 	public abstract void run();
+	
+	private static final String TABLE_ROW_XPATH = ".//tr";
 	
 	private WebDriver driver;
 }
