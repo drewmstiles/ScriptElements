0a501eef1b1d9742d063f9a7879b215ec17f5d67
drewmstiles <drewmstiles@gmail.com>
2017-04-21 19:15:37 -0700

    Remove Windows ChromeDriver workaround

--- src/drivers/DriverFactory.java
+++ src/drivers/DriverFactory.java
@@ -6,26 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DriverFactory
 {
 	public static final String CHROME = "chrome";
-	
-	private static final String WEB_DRIVER_CHROME = "webdriver.chrome.driver";
 
+	private static final String CHROME_DRIVER = "webdriver.chrome.driver";
 	private static final String DRIVERS_DIR = "/Users/dms/Developer/Java/ScriptElements/drivers/";
-	
 	private static final String CHROME_DRIVER_FILE = "chromedriver";
 	
-	private static final String EXE_EXTENSION = ".exe";
-	
 	public static WebDriver getDriverForBrowswer(String b)	{
-		
 		String driverPath = DRIVERS_DIR + CHROME_DRIVER_FILE;
-		try {
-			System.setProperty(WEB_DRIVER_CHROME, driverPath);
-		}
-		catch (IllegalStateException ex) {
-			// Exception present on Windows machines.
-			System.setProperty(WEB_DRIVER_CHROME, driverPath + EXE_EXTENSION);
-		}
-		
+		System.setProperty(CHROME_DRIVER, driverPath);
 		return new ChromeDriver();
 	}
 }
