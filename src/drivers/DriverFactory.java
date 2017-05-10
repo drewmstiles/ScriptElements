3e5495b7824eb7daae0378a2ffcf048d4701a9fc
drewmstiles <drewmstiles@gmail.com>
2017-05-09 23:32:31 -0700

    Update to Chrome WebDriver 2.29

--- src/drivers/DriverFactory.java
+++ src/drivers/DriverFactory.java
@@ -9,9 +9,10 @@ public class DriverFactory
 
 	private static final String CHROME_DRIVER = "webdriver.chrome.driver";
 	private static final String DRIVERS_DIR = "/Users/dms/Developer/Java/ScriptElements/drivers/";
-	private static final String CHROME_DRIVER_FILE = "chromedriver";
+	private static final String CHROME_DRIVER_FILE = "chromedriver-2-29";
 	
 	public static WebDriver getDriverForBrowswer(String b)	{
+		
 		String driverPath = DRIVERS_DIR + CHROME_DRIVER_FILE;
 		System.setProperty(CHROME_DRIVER, driverPath);
 		return new ChromeDriver();
