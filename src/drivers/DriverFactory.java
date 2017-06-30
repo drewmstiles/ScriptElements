6f5a41f7c6ff8e7506eed20f139bb62701bda8e2
drewmstiles <drewmstiles@gmail.com>
2017-06-29 22:11:52 -0700

    Use relative path for driver executable

--- src/drivers/DriverFactory.java
+++ src/drivers/DriverFactory.java
@@ -8,13 +8,11 @@ public class DriverFactory
 	public static final String CHROME = "chrome";
 
 	private static final String CHROME_DRIVER = "webdriver.chrome.driver";
-	private static final String DRIVERS_DIR = "/Users/dms/Developer/Java/ScriptElements/drivers/";
+	private static final String DRIVERS_DIR = "drivers/";
 	private static final String CHROME_DRIVER_FILE = "chromedriver-2-29";
 	
 	public static WebDriver getDriverForBrowswer(String b)	{
-		
-		String driverPath = DRIVERS_DIR + CHROME_DRIVER_FILE;
-		System.setProperty(CHROME_DRIVER, driverPath);
+		System.setProperty(CHROME_DRIVER, DRIVERS_DIR + CHROME_DRIVER_FILE);
 		return new ChromeDriver();
 	}
 }
