70b4e06650b2f0aaa368024722c4b9da651ec25b
drewmstiles <drewmstiles@gmail.com>
2017-07-11 09:25:09 -0700

    Update to Selenium 3.4.0

--- src/drivers/DriverFactory.java
+++ src/drivers/DriverFactory.java
@@ -2,6 +2,7 @@ package drivers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class DriverFactory
 {
@@ -12,7 +13,16 @@ public class DriverFactory
 	private static final String CHROME_DRIVER_FILE = "chromedriver";
 	
 	public static WebDriver getDriverForBrowswer(String b)	{
-		System.setProperty(CHROME_DRIVER, DRIVERS_DIR + CHROME_DRIVER_FILE);
-		return new ChromeDriver();
+
+		switch(b.toLowerCase()) {
+		case "chrome":
+			System.setProperty(CHROME_DRIVER, DRIVERS_DIR + CHROME_DRIVER_FILE);
+			return new ChromeDriver();
+		case "safari":
+			return new SafariDriver();
+		default:
+			System.out.printf("Error: '%s' does not denote any known browser.\n");
+			return null;
+		}
 	}
 }
